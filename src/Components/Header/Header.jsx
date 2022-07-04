13e832f9a3d80909228a2abec975e177e5ae7ed6
ntelio96 <mantas3456@gmail.com>
2022-07-04 17:11:42 +0300

    header changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import CTA from './CTA'
 import './header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mantas Siauciunas</h1>
+        <h5 className='text-light'>Junior Web Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="My Photo" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/Components/Header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin, BsGithub, BsDribbble} from 'react-icons/bs'
+
+function HeaderSocials() {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/mantas-siauciunas-78a7a064/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/ntelio96" target="_blank"><BsGithub/></a>
+        <a href="https://dribbble.com/#" target="_blank"><BsDribbble/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials

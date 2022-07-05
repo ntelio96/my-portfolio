10b3cbc512e043e077669ac5cf467f7181b37968
ntelio96 <mantas3456@gmail.com>
2022-07-05 15:12:02 +0300

    About me section complete

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,7 +3,22 @@ import './experience.css'
 
 function Experience() {
   return (
-    <section id='experience'> Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            
+          </div>
+        </div>
+        <div className='experience__backend'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 

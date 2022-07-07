1e45ab4c6f8559c7fe77b579094f3feb1183b0fd
ntelio96 <mantas3456@gmail.com>
2022-07-07 18:21:02 +0300

    contacts

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsMessenger, BsWhatsapp} from 'react-icons/bs'
 
 function Contact() {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Tauch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>mantas3456@gmail.com</h5>
+            <a href="mailto:mantas3456@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsMessenger className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Mantas Siauciunas</h5>
+            <a href="https://m.me/mantas.siauciunas.7/" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>mantas3456@gmail.com</h5>
+            <a href="https://api.whatsapp.com/send?phone=07878019768" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* end of contact options */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your email' required />
+          <textarea name="message" rows="7" placeholder='Your message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
